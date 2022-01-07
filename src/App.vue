0674f4ec5e299b318817143759fe674b917647e0
Vinci Taylaran <37849496+vincitaylaran@users.noreply.github.com>
2022-01-06 18:25:28 -0700

    Remove CSS variables from App.vue and create SCSS file for color variables

--- src/App.vue
+++ src/App.vue
@@ -24,17 +24,6 @@ body {
   margin: 0;
 }
 
-:root {
-  --orange: hsl(26, 100%, 55%);
-  --pale-orange: hsl(25, 100%, 94%);
-  --very-dark-blue: hsl(220, 13%, 13%);
-  --dark-grayish-blue: hsl(219, 9%, 45%);
-  --grayish-blue: hsl(220, 14%, 75%);
-  --light-grayish-blue: hsl(223, 64%, 98%);
-  --white: hsl(0, 0%, 100%);
-  --black: hsl(0, 0%, 0%);
-}
-
 #app {
   font-family: 'Kumbh Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
