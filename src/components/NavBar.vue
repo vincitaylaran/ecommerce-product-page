1bb7de75ea4f31a89658dbeb3f06be435295aabb
Vinci Taylaran <37849496+vincitaylaran@users.noreply.github.com>
2022-01-06 18:02:16 -0700

    Create mobile styles for nav menu

---
+++ src/components/NavBar.vue
@@ -0,0 +1,101 @@
+<template>
+  <div id="nav-bar">
+    <div class="left-container">
+      <img
+        v-on:click="toggleLinks"
+        class="menu"
+        src="../../assets/icon-menu.svg"
+        alt="menu"
+      />
+      <img class="logo" src="../../assets/logo.svg" alt="logo" />
+    </div>
+
+    <div class="right-container">
+      <img src="../../assets/icon-cart.svg" alt="cart" />
+      <img class="avatar" src="../../assets/image-avatar.png" alt="avatar" />
+    </div>
+
+    <div class="links" v-bind:class="{ hidden: isHidden }">
+      <img
+        v-on:click="toggleLinks"
+        src="../../assets/icon-close.svg"
+        alt="close"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  data: function () {
+    return {
+      isHidden: false,
+    }
+  },
+  methods: {
+    toggleLinks: function () {
+      if (this.isHidden) {
+        this.isHidden = false
+      } else {
+        this.isHidden = true
+      }
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+#nav-bar {
+  $nav-padding: 19px 24px 25px;
+
+  padding: $nav-padding;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  position: relative;
+
+  .avatar {
+    width: 24px;
+  }
+
+  .logo {
+    padding-bottom: 3px;
+  }
+
+  .left-container,
+  .right-container {
+    display: flex;
+    align-items: center;
+  }
+
+  .left-container {
+    img:first-child {
+      margin-right: 16px;
+    }
+  }
+
+  .right-container {
+    img:first-child {
+      margin-right: 22px;
+    }
+  }
+
+  .hidden {
+    display: none;
+  }
+
+  .links {
+    background: red;
+
+    position: absolute;
+    left: 0;
+    top: 0;
+    min-height: 100vh;
+    padding: $nav-padding;
+    width: 250px;
+  }
+}
+</style>
---
+++ src/components/TheLayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="the-layout">
+    <NavBar />
+    <slot />
+  </div>
+</template>
+
+<script>
+import NavBar from './NavBar.vue'
+
+export default {
+  name: 'TheLayout',
+  components: {
+    NavBar,
+  },
+}
+</script>
+
+<style scoped lang="scss">
+@import '../variables.scss';
+
+#the-layout {
+  padding: 28px 165px 132px;
+  @media (max-width: $breakpoint) {
+    padding: 0;
+    padding-bottom: 88px;
+  }
+}
+</style>
