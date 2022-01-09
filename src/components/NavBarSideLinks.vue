16f0c2467bba8c1365ced5571c7be376758df91b
Vinci Taylaran <37849496+vincitaylaran@users.noreply.github.com>
2022-01-08 20:45:29 -0700

    Create component from side links of nav bar when menu is clicked on mobile devices

---
+++ src/components/NavBarSideLinks.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- This element is hidden when viewing from devices wider than mobile phones -->
+  <div class="side-links" v-bind:class="{ hidden: isHidden }">
+    <img
+      v-on:click="toggleLinks"
+      src="../../assets/icon-close.svg"
+      alt="close"
+    />
+
+    <ul class="side-links__list">
+      <li v-for="link in links" v-bind:key="link">
+        {{ link }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBarSideLinks',
+  props: {
+    isHidden: Boolean,
+    links: Array,
+  },
+  methods: {
+    toggleLinks() {
+      this.$emit('close-side-links', true)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'NavBarSideLinks.scss';
+</style>
