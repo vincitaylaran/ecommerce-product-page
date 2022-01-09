3e11ef69219b54f851d8a46f9b987fe7acc1aa59
Vinci Taylaran <37849496+vincitaylaran@users.noreply.github.com>
2022-01-09 13:17:47 -0700

    Give UserCart a sample item again

--- src/components/NavBarRightContainer.vue
+++ src/components/NavBarRightContainer.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="right-container">
-    <UserCart />
+    <UserCart
+      :items="[
+        {
+          itemId: 1,
+          thumbnail: 'image-product-1-thumbnail.jpg',
+          name: 'Fall Limited Edition Sneakers',
+          basePrice: 125.0,
+          quantity: 3,
+        },
+      ]"
+    />
     <img class="avatar" src="../../assets/image-avatar.png" alt="avatar" />
   </div>
 </template>
