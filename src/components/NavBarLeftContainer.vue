f11b586e71fbecd28ec0227add49e75dab87dc23
Vinci Taylaran <37849496+vincitaylaran@users.noreply.github.com>
2022-01-08 20:45:11 -0700

    Create component for left container of nav bar

---
+++ src/components/NavBarLeftContainer.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="left-container">
+    <!-- This image/svg is only visible when viewing on mobile devices -->
+    <img
+      v-on:click="toggleLinks"
+      class="menu"
+      src="../../assets/icon-menu.svg"
+      alt="menu"
+    />
+    <img class="logo" src="../../assets/logo.svg" alt="logo" />
+    <ul class="links">
+      <li v-for="link in links" v-bind:key="link">
+        <a href=""
+          >{{ link }}
+          <div class="underline" />
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBarLeftContainer',
+  props: {
+    links: Array,
+    isHidden: Boolean,
+  },
+  methods: {
+    toggleLinks() {
+      this.$emit('open-side-links', false)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'NavBarLeftContainer.scss';
+</style>
