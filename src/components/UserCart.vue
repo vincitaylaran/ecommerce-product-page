4500679c45324b519f86e2692d8b4c7f6afd9e2a
Vinci Taylaran <37849496+vincitaylaran@users.noreply.github.com>
2022-01-09 11:38:15 -0700

    Rename CartIcon to UserCart.
    This is a more suitable name since this component will have an element of interactivity to it such as item count and clickability.

---
+++ src/components/UserCart.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="cart-icon-container">
+    <!-- TODO: create icon for cart items count -->
+    <svg
+      @click="toggleCart"
+      class="cart-icon"
+      width="22"
+      height="20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z"
+        fill="#69707D"
+        fill-rule="nonzero"
+      />
+    </svg>
+
+    <div class="basket">
+      <h4 class="basket__title">Cart</h4>
+      <div class="basket__items">
+        <div class="item">
+          <img src="" alt="" />
+          <p class="title"></p>
+          <span class="price-quantity"></span>
+          <span class="total"></span>
+          <button class="remove-item"></button>
+        </div>
+      </div>
+      <button class="checkout"></button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserCart',
+  methods: {
+    toggleCart() {
+      console.log('yo')
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+@import './UserCart.scss';
+</style>
