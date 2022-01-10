5e221c095fb2fb25d133a2f95cef2af31b0c0569
Vinci Taylaran <37849496+vincitaylaran@users.noreply.github.com>
2022-01-10 13:39:29 -0700

    Create a lightbox gallery component with prop types.

--- src/components/LightboxGallery.vue
+++ src/components/LightboxGallery.vue
@@ -1,9 +1,22 @@
 <template lang="">
-  <div>Lightbox</div>
+  <h1>yo</h1>
+  <img :src="fetchImage(product.images[0])" alt="" />
 </template>
-<script>
-export default {
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { Product } from '../interfaces'
+import helper from '../helper-functions'
+
+export default defineComponent({
   name: 'LightboxGallery',
-}
+  props: {
+    product: { type: Object as PropType<Product>, required: true },
+  },
+  methods: {
+    fetchImage() {
+      return helper.fetchImage(this.product.images[0])
+    },
+  },
+})
 </script>
-<style lang=""></style>
+<style lang="scss"></style>
